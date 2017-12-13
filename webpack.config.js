8dbc2628f9a3777d6c08c81c2398c0011dde489e
Osman Medzhytov <medjitov10@gmail.com>
2017-12-13 18:19:01 -0500

    qwe

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,26 @@
-var path = require('path');
-var webpack = require("webpack");
-
-var plugins = [];
-var devPlugins = [];
-
-var prodPlugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
-];
-
-plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-);
+const path = require('path');
 
 module.exports = {
-  entry: './front_end/src/index.js',
+  context: __dirname,
+  entry: './front_end/src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
-  plugins: plugins,
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
   module: {
     loaders: [
       {
-        test: [/\.jsx?$/],
-        exclude: /(node_modules)/,
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['react', 'env']
         }
       }
     ]
   },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx', '*']
-  }
+  devtool: 'source-maps',
 };
