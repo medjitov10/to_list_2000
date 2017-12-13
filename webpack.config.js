e5619f048cf7175c165b6e34b704d57443f44c93
Osman Medzhytov <medjitov10@gmail.com>
2017-12-13 10:13:19 -0500

    another try

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,33 @@
 var path = require('path');
+var webpack = require("webpack");
+
+var plugins = [];
+var devPlugins = [];
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
 
 module.exports = {
-  entry: './front_end/src/index.js',
+  entry: './frontend/src/index.js',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js',
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
